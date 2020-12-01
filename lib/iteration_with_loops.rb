77f265c7a3e85b7a2be9677cd9bc3a3271c9f47d
Nathan K Gusky <n.gusky88@gmail.com>
2020-12-01 19:18:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,6 @@
 def join_nested_strings(src)
 
-join_nested_strings = src.map(&:join).join('The quick brown fox studied Ruby')
+join_nested_strings = src.map(&:join).join('mixed_data_1')
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 end
